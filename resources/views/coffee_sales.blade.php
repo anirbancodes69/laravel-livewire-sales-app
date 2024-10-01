ed24556e5c13f69a78fbdb24403f237770c0e5c6
Anirban <anirbancodes@gmail.com>
2024-10-02 03:25:01 +0530

    part 1: added the form and performed the basic calculation using jquery and updated the view

--- resources/views/coffee_sales.blade.php
+++ resources/views/coffee_sales.blade.php
@@ -9,9 +9,67 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    @todo
+                    <form method="POST" action="#">
+                        @csrf
+                        <div class="form-group">
+                            <div>
+                                <label for="quantity">Quantity</label>
+                                <input type="number" name="quantity" id="quantity" value="{{ old('quantity') }}">
+                            </div>
+                            <div class="error">
+                                @error('quantity')
+                                {{ $message }}
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div>
+                                <label for="unit_cost">Unit Cost</label>
+                                <input type="number" name="unit_cost" id="unit_cost" value="{{ old('unit_cost') }}">
+                            </div>
+                            <div class="error">
+                                @error('unit_cost')
+                                {{ $message }}
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div>
+                                <label for="unit_cost">Selling Price: </label>
+                                <span id="selling_price"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div>
+                                <input type="submit" name="record_sale_btn" id="record_sale_btn" value="Record Sale">
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+
+    <x-slot name="scripts">
+        <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+        <script>
+            $(document).ready(function() {
+                function calculate() {
+                    const quantity = parseFloat($('#quantity').val());
+                    const unit_cost = parseFloat($('#unit_cost').val());
+
+                    if (!isNaN(quantity) && !isNaN(unit_cost)) {
+                        const cost = quantity * unit_cost;
+                        let sellingPrice = (cost / (1 - 0.25)) + 10;
+                        sellingPrice = sellingPrice.toFixed(2);
+                        $('#selling_price').text(`${sellingPrice}`);
+                    } else {
+                        $('#selling_price').text(''); // Clear result if any field is empty
+                    }
+                }
+                // Add input event listeners to both fields
+                $('#quantity, #unit_cost').on('input', calculate);
+            });
+        </script>
+    </x-slot>
 </x-app-layout>
